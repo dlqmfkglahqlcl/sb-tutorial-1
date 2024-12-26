b732a327003589fe36ba0e864e6da0b2ef7cc15c
dlqmfkglahqlcl <theshow0901@naver.com>
2024-12-26 14:20:33 +0900

    여러가지 리턴 예제

--- src/main/java/com/sbs/tutorial/controller/HomeController.java
+++ src/main/java/com/sbs/tutorial/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.*;
+
 @Controller
 
 public class HomeController {
@@ -46,5 +48,40 @@ public class HomeController {
     public int showPlus(@RequestParam(defaultValue = "0")int a, int b){
         return a + b;
     }
+    @GetMapping("/home/returnDouble")
+    @ResponseBody
+    public Double showReturnDouble() {
+        return Math.PI;
+    }
 
+    @GetMapping("/home/Array")
+    @ResponseBody
+    public int[] showReturnArray() {
+        int[] arr = new int[]{10, 20, 30};
+        return arr;
+    }
+
+    @GetMapping("/home/List")
+    @ResponseBody
+    public List<Integer> showReturnList() {
+        List<Integer> list = new ArrayList<>(){{
+            add(10);
+            add(20);
+            add(30);
+        }};
+        return list;
+    }
+
+    @GetMapping("/home/Map")
+    @ResponseBody
+    public Map<String, Object> showReturnMap() {
+        Map<String, Object>map = new LinkedHashMap<>();
+        map.put("id", 1);
+        map.put("subject", "제목1");
+        map.put("content", "내용1");
+        map.put("writerName", "김철수");
+        map.put("articleNo", new ArrayList<>(){{add(1);}});
+
+        return map;
+    }
 }
